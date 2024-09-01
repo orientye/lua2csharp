078163daae5e497fa245726ed18afefd742b4c3a
orientye <orientye@qq.com>
2024-09-01 20:05:37 +0800

    lua2csharp

--- src/main/resources/Exp.cs
+++ src/main/resources/Exp.cs
@@ -1,6 +1,19 @@
-int a = 2 + 3;
-int b = a - 1;
-int c = b * 2;
-int d = 16 + c / 5;
-bool m = false;
-bool n = m;
+// int
+int n1 = 2 + 3;            // n1 is 5
+int n2 = n1 - 1;           // n2 is 4
+int n3 = n2 * 2;           // n3 is 8
+float n4 = 16 + n3 / 5f;   // n4 is 17.6
+
+// float
+float f1 = 0.4f;           // f1 is a float with value 0.4
+float f2 = 4.57e-3f;       // f2 is a float with value 0.00457
+float f3 = 0.3e12f;        // f3 is a float with value 300000000000
+float f4 = 5e+20f;         // f4 is a float with value 500000000000000000000
+
+// bool
+bool b1 = false;           // b1 is a boolean set to false
+bool b2 = b1;              // b2 is also false (same as b1)
+
+// string
+string s1 = "i am a string"; // s1 is a string
+string s2 = s1 + " append";   // s2 is now "i am a string append"
--- src/main/resources/exp.lua
+++ src/main/resources/exp.lua
@@ -1,6 +1,19 @@
-local a = 2 + 3;
-local b = a - 1;
-local c = b * 2;
-local d = 16 + c / 5;
-local m = false;
-local n = m;
+-- int
+local n1 = 2 + 3;
+local n2 = n1 - 1;
+local n3 = n2 * 2;
+local n4 = 16 + n3 / 5;
+
+-- float
+local f1 = 0.4
+local f2 = 4.57e-3
+local f3 = 0.3e12
+local f4 = 5e+20
+
+-- bool
+local b1 = false;
+local b2 = b1;
+
+-- string
+local s1 = "i am a string"
+local s2 = s1 + " append"
