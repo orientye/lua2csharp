0d6c744d1ab78000fe18e0f532f6922c60598cee
orientye <orientye@qq.com>
2024-09-07 07:04:38 +0800

    lua2csharp

--- src/main/java/org/orient/Main.java
+++ src/main/java/org/orient/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("\n===================================\n");
 
-        InputStream inStream = Main.class.getClassLoader().getResourceAsStream("table.lua");
+        InputStream inStream = Main.class.getClassLoader().getResourceAsStream("return.lua");
         assert (inStream != null);
         CharStream charStream = CharStreams.fromStream(inStream);
 
