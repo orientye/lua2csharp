4040a35d28b02b7b5ed6a0b2b2cd5f6bcd1fcc25
orientye <orientye@qq.com>
2024-09-10 07:28:31 +0800

    lua2csharp

--- src/test/java/org/orient/TestMain.java
+++ src/test/java/org/orient/TestMain.java
@@ -1,62 +1,37 @@
 package org.orient;
 
 import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class TestMain {
     public static void main(String[] args) throws Exception {
-        Path path = Paths.get("./src/test/examples");
-        if (Files.exists(path) && Files.isDirectory(path)) {
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
-                for (Path entry : stream) {
-                    System.out.println(entry.getFileName());
-                }
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
-        } else {
-            System.out.println("not exist or not directory ");
-        }
 
-//        List<String> txtFiles = scanFiles("./src/test/examples", ".lua");
-//
-//        for (String f : txtFiles) {
-//            System.out.println(f);
-//        }
-
-//        String fileName = "";
-//        String result = Transform.transformFromFileName(fileName);
+        File folder = new File("./src/test/examples");
+        traverseFolder(folder);
     }
 
-    private static void testTransform() {
-
-    }
-
-    public static List<String> scanFiles(String directoryPath, String fileExtension) {
-        List<String> filePaths = new ArrayList<>();
-        File directory = new File(directoryPath);
-
-        if (directory.exists() && directory.isDirectory()) {
-            FilenameFilter filter = (dir, name) -> name.endsWith(fileExtension);
-            File[] files = directory.listFiles(filter);
-
-            if (files != null) {
-                for (File file : files) {
-                    filePaths.add(file.getAbsolutePath());
+    public static void traverseFolder(File folder) {
+        if (folder.isDirectory()) {
+            File[] listOfFiles = folder.listFiles();
+            if (listOfFiles != null) {
+                for (File listOfFile : listOfFiles) {
+                    if (listOfFile.isFile()) {
+                        String fileName = listOfFile.getName();
+                        if (fileName.endsWith("lua")) {
+                            System.out.println("lua: " + listOfFile.getName());
+                            System.out.println("lua: " + listOfFile.getAbsolutePath());
+                            System.out.println("lua: " + listOfFile.getPath());
+                        } else if (fileName.endsWith("cs")) {
+                            System.out.println("cs:  " + listOfFile.getName());
+                            System.out.println("cs:  " + listOfFile.getAbsolutePath());
+                            System.out.println("cs:  " + listOfFile.getPath());
+                        }
+                    } else if (listOfFile.isDirectory()) {
+                        traverseFolder(listOfFile);
+                    }
                 }
             }
         } else {
-            System.out.println("not exist or not directory ");
+            System.err.println(folder.getName() + " is not a directory");
         }
-
-        return filePaths;
     }
-
 }
