8d5ea552f88a07c596bee330da75267f45364aea
orientye <orientye@qq.com>
2024-09-22 19:52:37 +0800

    lua2csharp

--- src/test/java/org/orient/TestMain.java
+++ src/test/java/org/orient/TestMain.java
@@ -8,10 +8,10 @@ public class TestMain {
     public static void main(String[] args) throws Exception {
         HashSet<String> excludes = new HashSet<>();
         excludes.add("Class.lua");
-        excludes.add("CustomClass.lua");
+        //excludes.add("CustomClass.lua");
         excludes.add("VarNil.lua");
 
-        File file = new File("./src/test/examples/");
+        File file = new File("./src/test/examples/CustomClass.lua");
         doFile(file, excludes);
     }
 
