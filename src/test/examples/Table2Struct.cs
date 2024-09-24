fada021cb75772000665aa584c34b5744a38dac7
orientye <orientye@qq.com>
2024-09-25 07:27:12 +0800

    lua2csharp

---
+++ src/test/examples/Table2Struct.cs
@@ -0,0 +1,5 @@
+public class ProjectDef
+{
+    public const int ID = 1;
+    public const string NAME = "abc";
+}
---
+++ src/test/examples/Table2Struct.lua
@@ -0,0 +1,6 @@
+local ProjectDef = {}
+
+ProjectDef.ID = 1;
+ProjectDef.NAME = "abc";
+
+return ProjectDef
