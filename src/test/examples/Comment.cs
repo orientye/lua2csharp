ab6cdd425e976cc590730320e392f8c876322357
orientye <orientye@qq.com>
2024-10-18 09:29:22 +0800

    lua2csharp

--- src/test/examples/Comment.cs
+++ src/test/examples/Comment.cs
@@ -3,6 +3,7 @@ int i = 10;
 // second comment
 Console.WriteLine(i);
 Console.WriteLine(i + 1); // third comment
+Console.WriteLine(i + 2);
 /**
 multi comment line 1
 multi comment line 2
--- src/test/examples/Comment.lua
+++ src/test/examples/Comment.lua
@@ -1,3 +1,9 @@
+--first comment
+local i = 10;
+--second comment
+print(i);
+print(i+1); -- third comment
+print(i+2);
 --[[
 multi comment line 1
 multi comment line 2
