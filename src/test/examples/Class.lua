2c42f8d34d5422e14e196d47ec86a7d09aeb706d
orientye <orientye@qq.com>
2024-10-19 17:29:42 +0800

    lua2csharp

--- src/test/examples/Class.lua
+++ src/test/examples/Class.lua
@@ -5,4 +5,12 @@ function t:sayHello()
     print("Second" .. t.name)
 end
 
+t2 = { name = "Mike" }
+function t.sayWorld(self, atable)
+    print("Hello" .. self.name)
+    print("Second" .. t.name)
+    print("Third" .. atable.name)
+end
+
 t:sayHello()
+t.sayWorld(t, t2)
