1c272e5b2e5a6dfd0e7415fddf13528020044f08
orientye <orientye@qq.com>
2024-10-23 07:20:09 +0800

    lua2csharp

--- src/main/java/org/orient/PassScopeAndType.java
+++ src/main/java/org/orient/PassScopeAndType.java
@@ -393,7 +393,12 @@ public class PassScopeAndType extends LuaParserBaseListener {
                 String name = ctx.getText();
                 Symbol.create(name, st, ctx, this.annotatedTree);
             } else {
-                System.out.println("Unresolved table: " + ctx.getText());
+                if (this.passingTimes > 1)
+                {
+                    String name = ctx.getText();
+                    Symbol.create(name, Symbol.Type.SYMBOL_TYPE_CLASS, ctx, this.annotatedTree);
+                    System.out.println("Unresolved table: " + ctx.getText());
+                }
             }
         }
     }
