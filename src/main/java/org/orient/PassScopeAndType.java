0cb21feb6bf30ee399909815bed85731336cbfa4
orientye <orientye@qq.com>
2024-10-26 09:29:00 +0800

    lua2csharp

--- src/main/java/org/orient/PassScopeAndType.java
+++ src/main/java/org/orient/PassScopeAndType.java
@@ -707,6 +707,7 @@ public class PassScopeAndType extends LuaParserBaseListener {
      */
     @Override
     public void exitField(LuaParser.FieldContext ctx) {
+        System.out.println("FieldContext text: " + ctx.getText());
     }
 
     /**
