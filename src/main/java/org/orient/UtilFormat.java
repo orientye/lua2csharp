2b8f3065f15de8348dab8fe1072ca2c8b9b901e0
orientye <orientye@qq.com>
2024-11-02 13:09:10 +0800

    lua2csharp

---
+++ src/main/java/org/orient/UtilFormat.java
@@ -0,0 +1,16 @@
+package org.orient;
+
+public class UtilFormat {
+
+    private static void TryAddSemicolon(LuaParser.StatContext ctx) {
+
+    }
+
+    private static boolean IsNecessaryAddSemicolon(LuaParser.StatContext ctx) {
+        return false;
+    }
+
+    private static void AddSemicolon(LuaParser.StatContext ctx) {
+
+    }
+}
