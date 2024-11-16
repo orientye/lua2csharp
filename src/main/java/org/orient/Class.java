ba50eda24981cb97c3485f5184e7c42a195db6d7
orientye <orientye@qq.com>
2024-11-16 18:03:16 +0800

    lua2csharp

--- src/main/java/org/orient/Class.java
+++ src/main/java/org/orient/Class.java
@@ -7,6 +7,8 @@ public class Class {
     private final String name;
     final Map<String, Symbol> fields = new LinkedHashMap<>();
 
+    final Map<String, Symbol> functions = new LinkedHashMap<>();
+
     public Class(String name) {
         this.name = name;
     }
