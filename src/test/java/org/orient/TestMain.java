ef0a8fc1a3213649832fcd9a8da908e5cbf8acb4
orientye <orientye@qq.com>
2025-01-30 16:25:02 +0800

    lua2csharp

--- src/test/java/org/orient/TestMain.java
+++ src/test/java/org/orient/TestMain.java
@@ -19,7 +19,7 @@ public class TestMain {
         excludes.add("Table2Struct.lua");
         excludes.add("VarNil.lua");
 
-        File file = new File("./src/test/examples/Class.lua");
+        File file = new File("./src/test/examples/");
         if (file.isFile()) {
             String fileName = file.getName();
             excludes.remove(fileName);
